75df13542401fa0ac49449e99ae9f3c954fece55
KMacKenzie <kenny@mackenzie.ai>
2020-04-04 00:41:01 -0700

    Updates

---
+++ precache-manifest.5afa922ddd361c9ba3a643514fefe59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be82caff2cd0bead1b6df77005d19cbb",
+    "url": "/monsters.rolodex/index.html"
+  },
+  {
+    "revision": "6569108d685ccf30fc23",
+    "url": "/monsters.rolodex/static/css/main.e3fc61c1.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters.rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters.rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6569108d685ccf30fc23",
+    "url": "/monsters.rolodex/static/js/main.6fc4a22c.chunk.js"
+  },
+  {
+    "revision": "6800d62d3f7699923bcc",
+    "url": "/monsters.rolodex/static/js/runtime-main.98c77370.js"
+  }
+]);
